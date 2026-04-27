47253062cb85f95fd3ea7de445a2e357f7247903
zero0zero7 <limxinyi7@gmail.com>
2026-04-28 00:02:54 +0100

    Alpha equivalence check implementation

---
+++ src/main/kotlin/com/wgslfuzz/core/AlphaEquivalence.kt
@@ -0,0 +1,209 @@
+package com.wgslfuzz.core
+
+
+
+/**
+ * Parses an AstNode to return a characteristic vector
+ */
+private fun characteristicVectorFromAstnode(
+    node: AstNode,
+    variableNames: MutableList<String>,
+): List<String> {
+
+    fun addVariableName(name: String) = variableNames.add(name)
+
+    when (node) {
+        // Attributes
+//        is Attribute.Align -> characteristicVectorFromAstnode(node.expression, variableNames)
+//        is Attribute.Binding -> characteristicVectorFromAstnode(node.expression, variableNames)        is Attribute.Align -> characteristicVectorFromAstnode(node.expression, variableNames)
+//        is Attribute.BlendSrc -> characteristicVectorFromAstnode(node.expression, variableNames)
+//        is Attribute.Builtin -> addVariableName(node.name)
+//        is Attribute.Group -> characteristicVectorFromAstnode(node.expression, variableNames)
+//        is Attribute.Location -> characteristicVectorFromAstnode(node.expression, variableNames)
+//        is Attribute.Size -> characteristicVectorFromAstnode(node.expression, variableNames)
+//        is Attribute.WorkgroupSize -> {
+//            characteristicVectorFromAstnode(node.sizeX, variableNames)
+//            node.sizeY?.let {characteristicVectorFromAstnode(node.sizeY, variableNames)}
+//            node.sizeZ?.let {characteristicVectorFromAstnode(node.sizeZ, variableNames)}
+//        }
+//        is Attribute.InputAttachmentIndex -> characteristicVectorFromAstnode(node.expression, variableNames)
+
+        // GlobalDecl
+        is GlobalDecl.Constant -> addVariableName(node.name)
+//        is GlobalDecl.Override -> addVariableName(node.name)
+        is GlobalDecl.Variable -> addVariableName(node.name)
+        is GlobalDecl.Function -> {
+            for (param in node.parameters) {
+                characteristicVectorFromAstnode(param, variableNames)
+            }
+            for (attri in node.returnAttributes) {
+                characteristicVectorFromAstnode(attri, variableNames)
+            }
+            characteristicVectorFromAstnode(node.body, variableNames)
+        }
+//        is GlobalDecl.ConstAssert -> characteristicVectorFromAstnode(node.expression, variableNames)
+
+        // Expression
+        is Expression.Identifier -> addVariableName(node.name)
+
+        is Expression.Paren -> characteristicVectorFromAstnode(node.target, variableNames)
+        is Expression.Unary -> characteristicVectorFromAstnode(node.target, variableNames)
+         is LhsExpression.Paren -> characteristicVectorFromAstnode(node.target, variableNames )
+
+        is Expression.Binary -> {
+            characteristicVectorFromAstnode(node.lhs, variableNames)
+            characteristicVectorFromAstnode(node.rhs, variableNames)
+        }
+
+        is Expression.FunctionCall -> node.args.forEach { characteristicVectorFromAstnode(it, variableNames) }
+        is Expression.ValueConstructor -> {
+            if (node is Expression.ArrayValueConstructor) {
+                node.elementCount?.let { characteristicVectorFromAstnode(node.elementCount, variableNames) }
+            }
+            node.args.forEach { characteristicVectorFromAstnode(it, variableNames) }
+        }
+
+        is Expression.MemberLookup -> {
+            addVariableName(node.memberName) // TODO: check
+            characteristicVectorFromAstnode(node.receiver, variableNames)
+        }
+
+        is Expression.IndexLookup -> {
+            characteristicVectorFromAstnode(node.target, variableNames)
+            characteristicVectorFromAstnode(node.index, variableNames)
+        }
+
+        // LhsExpression
+        is LhsExpression.Identifier -> addVariableName(node.name)
+        is LhsExpression.Paren -> characteristicVectorFromAstnode(
+            node.target,
+            variableNames
+        )
+        is LhsExpression.Dereference -> characteristicVectorFromAstnode(
+            node.target,
+            variableNames
+        )
+        is LhsExpression.AddressOf -> characteristicVectorFromAstnode(
+            node.target,
+            variableNames
+        )
+        is LhsExpression.MemberLookup -> {
+            addVariableName(node.memberName) // TODO: check
+            characteristicVectorFromAstnode(node.receiver, variableNames)
+        }
+        is LhsExpression.IndexLookup -> {
+            characteristicVectorFromAstnode(node.target, variableNames)
+            characteristicVectorFromAstnode(node.index, variableNames)
+        }
+
+        // Statement
+        is Statement.Return -> {
+            node.expression?.let { characteristicVectorFromAstnode(it, variableNames) }
+        }
+
+        is Statement.Assignment -> {
+            node.lhsExpression?.let { characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.rhs, variableNames)
+        }
+
+        is Statement.Increment -> characteristicVectorFromAstnode(node.target, variableNames)
+        is Statement.Decrement -> characteristicVectorFromAstnode(node.target, variableNames)
+        is Statement.ConstAssert -> characteristicVectorFromAstnode(node.expression, variableNames)
+        is Statement.Compound -> node.statements.forEach { characteristicVectorFromAstnode(it, variableNames) }
+        is Statement.If -> {
+            node.attributes.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.condition, variableNames)
+            characteristicVectorFromAstnode(node.thenBranch, variableNames)
+            node.elseBranch?.let { characteristicVectorFromAstnode(it, variableNames) }
+        }
+
+        is Statement.Switch -> {
+            node.attributesAtStart.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            node.attributesBeforeBody.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            node.clauses.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.expression, variableNames)
+        }
+
+        is Statement.Loop -> {
+            node.attributesAtStart.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            node.attributesBeforeBody.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.body, variableNames)
+            node.continuingStatement?.let { characteristicVectorFromAstnode(it, variableNames) }
+        }
+
+        is Statement.For -> {
+            node.attributes.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            node.init?.let { characteristicVectorFromAstnode(it, variableNames) }
+            node.condition?.let { characteristicVectorFromAstnode(it, variableNames) }
+            node.update?.let { characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.body, variableNames)
+        }
+
+        is Statement.While -> {
+            node.attributes.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.body, variableNames)
+            characteristicVectorFromAstnode(node.condition, variableNames)
+        }
+
+        is Statement.FunctionCall -> node.args.forEach {
+            characteristicVectorFromAstnode(
+                it,
+                variableNames
+            )
+        } // TODO: is callee function name, or variable name. x.add?
+        is Statement.Value -> {
+            addVariableName(node.name)
+            characteristicVectorFromAstnode(node.initializer, variableNames)
+        }
+
+        is Statement.Variable -> {
+            addVariableName(node.name)
+            node.initializer?.let { characteristicVectorFromAstnode(it, variableNames) }
+        }
+
+        is ContinuingStatement -> {
+            node.attributes.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.statements, variableNames)
+            node.breakIfExpr?.let { characteristicVectorFromAstnode(it, variableNames) }
+        }
+
+        is SwitchClause -> {
+            node.caseSelectors.filterNotNull().forEach {characteristicVectorFromAstnode(it, variableNames) }
+            characteristicVectorFromAstnode(node.compoundStatement, variableNames)
+        }
+
+        is ParameterDecl -> {
+            node.attributes.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            addVariableName(node.name)
+        }
+        is StructMember -> {
+            node.attributes.forEach { characteristicVectorFromAstnode(it, variableNames) }
+            addVariableName(node.name)
+        }
+
+        // Attribute: Compute, Const, Diagnostic, Fragment, Interpolate, Invariant, MustUse, Vertex
+        // Directive
+        // GlobalDecl: Struct, TypeAlias, Empty
+        // TypeDecl (all)
+        // Expression: BoolLiteral, FloatLiteral, IntLiteral
+        // Statement: Empty, Break, Continue, Discard
+        else -> {}
+    }
+    traverse(::characteristicVectorFromAstnode, node, variableNames)
+    return variableNames
+}
+
+// TODO: this wouldnt work for more than 10 variables as 2 same restrict growth strings can have different variable allocation. eg. 0112 could be 0.11.2 or 0.1.12
+fun restrictGrowthStr(
+    charVect: List<String>
+) : String {
+    val result = StringBuilder()
+    val map = mutableMapOf<String, Int>()
+    var counter = 0
+    for (varName in charVect) {
+        val temp = map.getOrPut(varName) { counter++ }
+        result.append(temp)
+    }
+    return result.toString()
+}
+
