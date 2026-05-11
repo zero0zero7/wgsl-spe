8318c1915056978dfe8cc3af14ed826634a72800
zero0zero7 <limxinyi7@gmail.com>
2026-05-11 21:06:25 +0100

    Integrate wgslmith to pipeline

--- src/test/kotlin/com/wgslspe/core/SkeletalEnumeratorTests.kt
+++ src/test/kotlin/com/wgslspe/core/SkeletalEnumeratorTests.kt
@@ -303,7 +303,6 @@ class SkeletalEnumeratorTests {
         """.trimIndent()
         val tu = parseFromString(src, LoggingParseErrorListener())
         val env = resolve(tu)
-        val (decls, usages) = collectSkeletalCandidates(tu, env)
         val skeletons = allReplacementSkeletons(tu, env).toList()
         // Replacements
         // Lhs of Assignment can only be "result" or "a", not "b"
